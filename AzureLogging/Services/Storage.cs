d9f1e7252aec088db838b6aa579b3c88052d1b66
Rūdolfs K <8761347+RudolfKay@users.noreply.github.com>
2022-10-25 22:12:50 +0300

    Added BlobClient init. as well as BlobStorage code

---
+++ AzureLogging/Services/Storage.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.VisualBasic;
+using System.Security.Cryptography;
+
+namespace AzureLogging.Services
+{
+    public class Storage
+    {
+        private readonly BlobContainerClient _blobContainerClient;
+
+        public Storage()
+        {
+            var client = new BlobServiceClient("UseDevelopmentStorage=true");
+
+            try
+            {
+                _blobContainerClient = client.CreateBlobContainer("AzureLoggingBlob");
+            }
+            catch
+            {
+                _blobContainerClient = client.GetBlobContainerClient("AzureLoggingBlob");
+            }
+        }
+    }
+}
